a74832ff19a5ff2289734e5b133008ef7e393824
BackGwa <backgwa@icloud.com>
2023-05-20 13:06:05 +0900

    Update search.js

--- script/search.js
+++ script/search.js
@@ -1,6 +1,4 @@
 
-document.onkeydown()
-
 function goSearch(){
     query = document.getElementById("search").value;
     if(query) location.href = `https://www.youtube.com/results?search_query=${query}`;
