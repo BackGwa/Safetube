e353ef38bc77dad03158ee78ba55c2782c633a62
BackGwa <backgwa@icloud.com>
2023-05-20 13:04:49 +0900

    Enter로 검색 추가

--- script/search.js
+++ script/search.js
@@ -1,5 +1,14 @@
 
-function gosearch(){
+document.onkeydown()
+
+function goSearch(){
     query = document.getElementById("search").value;
     if(query) location.href = `https://www.youtube.com/results?search_query=${query}`;
+}
+
+function enterSearch(e){
+    if(e.keyCode == 13){
+        goSearch();
+        return false;
+    } else return true;
 }
